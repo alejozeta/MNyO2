3bdb0cb50eb663bb14407550bcdabe0185cabd80
timomenceyra <tmenceyra@udesa.edu.ar>
2024-06-02 23:27:34 -0300

    Update ejercicio2.py

--- TP3/Ejercicio 2/ejercicio2.py
+++ TP3/Ejercicio 2/ejercicio2.py
@@ -35,6 +35,7 @@ for d in compression_values:
     U_d = U[:, :d]
     S_d = np.diag(S[:d])
     Vt_d = Vt[:d, :]
+    
     image_approximation = U_d @ S_d @ Vt_d
 
     plt.figure(figsize=(10, 5))
@@ -76,3 +77,32 @@ for d in compression_values:
 images_2 = ['datasets_imgs_02/img00.jpeg', 'datasets_imgs_02/img01.jpeg', 'datasets_imgs_02/img02.jpeg', 'datasets_imgs_02/img03.jpeg', 
             'datasets_imgs_02/img04.jpeg', 'datasets_imgs_02/img05.jpeg', 'datasets_imgs_02/img06.jpeg', 'datasets_imgs_02/img07.jpeg']
 
+def frobenius_norm(matrix):
+    return np.linalg.norm(matrix, 'fro')
+
+images_dataset2 = []
+for img in images_2:
+    image_as_matrix = imread(img)
+    images_dataset2.append(image_as_matrix.flatten())
+
+images_dataset2 = np.array(images_dataset2)
+
+# Descomposición SVD
+U_dtst2, S_dtst2, Vt_dtst2 = np.linalg.svd(images_dataset2, full_matrices=False)
+
+for d in range(1, 8):
+    U_d_dtst2 = U_dtst2[:, :d]
+    S_d_dtst2 = np.diag(S_dtst2[:d])
+    Vt_d_dtst2 = Vt_dtst2[:d, :]
+    
+    image_approximation = U_d_dtst2 @ S_d_dtst2 @ Vt_d_dtst2
+
+    frobenius = frobenius_norm(images_dataset2 - image_approximation)
+    print(f'Error para d = {d}: {frobenius * 100}%')
+    if frobenius < 0.1:
+        print(f'El número mínimo de dimensiones que genera menos de 10% de error en la reducción es {d}')
+        print(f'Error: {frobenius * 100}%')
+        break
+    
+    # elif d == 7:
+    #     print(f'No se encontró un número de dimensiones que genere menos de 10% de error en la reducción.  {frobenius * 100}%')
